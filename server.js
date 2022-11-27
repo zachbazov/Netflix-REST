0cc248994c177bc73f87afe49c3b54d9c9fd3b49
Zach Bazov <zachbazov@gmail.com>
2022-11-28 00:31:24 +0200

    Changing configuration for AWS deployment.

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
 
-// const db = process.env.DB_URL.replace(
-//     /<DB_USER>|<DB_PASS>|<DB_CLUSTER>|<DB_NAME>/gi,
-//     (arg) => {
-//         return {
-//             '<DB_USER>': process.env.DB_USER,
-//             '<DB_PASS>': process.env.DB_PASS,
-//             '<DB_CLUSTER>': process.env.DB_CLUSTER,
-//             '<DB_NAME>': process.env.DB_NAME
-//         }[arg];
-//     }
-// );
+const db = process.env.DB_URL.replace(
+    /<DB_USER>|<DB_PASS>|<DB_CLUSTER>|<DB_NAME>/gi,
+    (arg) => {
+        return {
+            "<DB_USER>": process.env.DB_USER,
+            "<DB_PASS>": process.env.DB_PASS,
+            "<DB_CLUSTER>": process.env.DB_CLUSTER,
+            "<DB_NAME>": process.env.DB_NAME,
+        }[arg];
+    }
+);
 
-const db = `mongodb+srv://zachbazov:zachbazovr123@cluster0.bjjiy.mongodb.net/?retryWrites=true&w=majority`;
+// const db = `mongodb+srv://zachbazov:zachbazovr123@cluster0.bjjiy.mongodb.net/?retryWrites=true&w=majority`;
 
 mongoose
     .connect(db, {
